8a6078c635d8a3df9e85be4d18ebf1fa0c09b3da
Mbring jeremie <mbringjeremys@gmail.com>
2022-04-12 21:36:24 +0100

    integreted firebase into app, added sign in and sign out with google account

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import { Switch, Route, Link } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 import './App.css';
 class App extends React.Component {
   constructor(props){
      super(props);
-  } 
+
+     this.state = {
+       currentUser: null
+     };
+  }
+
+  unSubscribeFromAuth = null
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({
+        currentUser: user
+      })
+      console.log(user)
+    })
+  }
+  
+  componentWillUnmount() {
+    this.unSubscribeFromAuth();
+  }
+
   render() {
     return (
       <div>
-        <Header />
+        <Header currentUser={this.state.currentUser}/>
         <Switch>
           <Route exact path='/'>
             <Homepage />
